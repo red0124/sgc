6053e03cc676db16a122a8f5c43570354881df71
ado <adnan.abd0124@gmail.com>
2017-02-11 03:05:11 +0100

    implement vector

--- lib/vector.h
+++ lib/vector.h
@@ -23,8 +23,8 @@
                 free(vec->_vector_elements);                                   \
         }                                                                      \
                                                                                \
-        int vec_set_##type_name(vector_##type_name *vec, int num,              \
-                                type_name el)                                  \
+        int vec_set_##type_name(vector_##type_name *vec, const int num,        \
+                                const type_name el)                            \
         {                                                                      \
                 int ret = 0;                                                   \
                 if(num < vec->_vector_size)                                    \
@@ -35,7 +35,8 @@
                 return ret;                                                    \
         }                                                                      \
                                                                                \
-        void vec_push_back_##type_name(vector_##type_name *vec, type_name el)  \
+        void vec_push_back_##type_name(vector_##type_name *vec,                \
+                                       const type_name el)                     \
         {                                                                      \
                 if(vec->_vector_max == vec->_vector_size)                      \
                 {                                                              \
@@ -53,7 +54,8 @@
                 return (vec->_vector_elements)[--vec->_vector_size];           \
         }                                                                      \
                                                                                \
-        type_name vec_get_##type_name(vector_##type_name *vec, int num)        \
+        type_name vec_get_##type_name(const vector_##type_name *vec,           \
+                                      const int num)                           \
         {                                                                      \
                 type_name ret;                                                 \
                 if(num < vec->_vector_size && num >= 0)                        \
@@ -66,7 +68,7 @@
                 }                                                              \
                 return ret;                                                    \
         }                                                                      \
-        int vec_insert_##type_name(vector_##type_name *vec, int num,           \
+        int vec_insert_##type_name(vector_##type_name *vec, const int num,     \
                                    type_name el)                               \
         {                                                                      \
                 int ret = 0;                                                   \
@@ -84,23 +86,8 @@
                 return ret;                                                    \
         }                                                                      \
                                                                                \
-        int vec_erase_##type_name(vector_##type_name *vec, int num)            \
-        {                                                                      \
-                int ret = 0;                                                   \
-                if(num < vec->_vector_size && num >= 0)                        \
-                {                                                              \
-                        ret = 1;                                               \
-                        for(int i = num; i < vec->_vector_size - 1; i++)       \
-                        {                                                      \
-                                (vec->_vector_elements)[i] =                   \
-                                    (vec->_vector_elements)[i + 1];            \
-                        }                                                      \
-                        vec->_vector_size--;                                   \
-                }                                                              \
-                return ret;                                                    \
-        }                                                                      \
-                                                                               \
-        int vec_find_##type_name(vector_##type_name *vec, type_name el)        \
+        int vec_find_##type_name(const vector_##type_name *vec,                \
+                                 const type_name el)                           \
         {                                                                      \
                 int ret = -1;                                                  \
                 for(int i = 0; i < vec->_vector_size; i++)                     \
@@ -116,4 +103,141 @@
         int vec_size_##type_name(vector_##type_name *vec)                      \
         {                                                                      \
                 return vec->_vector_size;                                      \
+        }                                                                      \
+                                                                               \
+        int vec_copy_interval_##type_name(vector_##type_name *to,              \
+                                          const vector_##type_name *from,      \
+                                          const int x, const int y)            \
+        {                                                                      \
+                int begin = (x > y) ? y : x;                                   \
+                begin = (begin > 0) ? begin : 0;                               \
+                int end = (x > y) ? x : y;                                     \
+                end = (end < from->_vector_size) ? end : from->_vector_size;   \
+                for(int i = begin; i < end && i < from->_vector_size; i++)     \
+                {                                                              \
+                        vec_push_back_##type_name(                             \
+                            to, (from->_vector_elements)[i]);                  \
+                }                                                              \
+                return (x > from->_vector_size || x < 0 ||                     \
+                        y > from->_vector_size || y < 0)                       \
+                           ? 1                                                 \
+                           : 0;                                                \
+        }                                                                      \
+                                                                               \
+        int vec_copy_##type_name(vector_##type_name *to,                       \
+                                 const vector_##type_name *from)               \
+        {                                                                      \
+                return vec_copy_interval_##type_name(to, from, 0,              \
+                                                     from->_vector_size);      \
+        }                                                                      \
+                                                                               \
+        int vec_erase_interval_##type_name(vector_##type_name *vec,            \
+                                           const int x, const int y)           \
+        {                                                                      \
+                int begin = (x > y) ? y : x;                                   \
+                begin = (begin > 0) ? begin : 0;                               \
+                int end = (x > y) ? x : y;                                     \
+                end = (end < vec->_vector_size) ? end : vec->_vector_size;     \
+                                                                               \
+                for(int i = begin; i < vec->_vector_size - (end - begin); i++) \
+                {                                                              \
+                        (vec->_vector_elements)[i] =                           \
+                            (vec->_vector_elements)[i + (end - begin)];        \
+                }                                                              \
+                vec->_vector_size -= end - begin;                              \
+                                                                               \
+                return (x > vec->_vector_size || x < 0 ||                      \
+                        y > vec->_vector_size || y < 0)                        \
+                           ? 1                                                 \
+                           : 0;                                                \
+        }                                                                      \
+                                                                               \
+        int vec_erase_##type_name(vector_##type_name *vec, const int num)      \
+        {                                                                      \
+                return vec_erase_interval_##type_name(vec, num, num + 1);      \
+        }                                                                      \
+                                                                               \
+        void swap_##type_name(type_name *a, type_name *b)                      \
+        {                                                                      \
+                type_name tmp = *a;                                            \
+                *a = *b;                                                       \
+                *b = tmp;                                                      \
+        }                                                                      \
+                                                                               \
+        void vec_sort_interval_##type_name(                                    \
+            vector_##type_name *vec, int (*condition)(type_name, type_name),   \
+            const int x, const int y)                                          \
+        {                                                                      \
+                int begin = (x > y) ? y : x;                                   \
+                begin = (begin > 0) ? begin : 0;                               \
+                int end = (x > y) ? x : y;                                     \
+                end = (end < vec->_vector_size) ? end : vec->_vector_size;     \
+                                                                               \
+                int finished = 0;                                              \
+                while(!finished)                                               \
+                {                                                              \
+                        finished = 1;                                          \
+                        for(int i = begin; i < end - 1; i++)                   \
+                        {                                                      \
+                                if(condition((vec->_vector_elements[i]),       \
+                                             (vec->_vector_elements[i + 1])))  \
+                                {                                              \
+                                        swap_##type_name(                      \
+                                            &(vec->_vector_elements)[i],       \
+                                            &(vec->_vector_elements)[i + 1]);  \
+                                        finished = 0;                          \
+                                }                                              \
+                        }                                                      \
+                }                                                              \
+        }                                                                      \
+                                                                               \
+        void vec_sort_##type_name(vector_##type_name *vec,                     \
+                                  int (*condition)(type_name, type_name))      \
+        {                                                                      \
+                vec_sort_interval_##type_name(vec, condition, 0,               \
+                                              vec->_vector_size);              \
+        }                                                                      \
+                                                                               \
+        int default_condition_##type_name(type_name a, type_name b)            \
+        {                                                                      \
+                return a > b;                                                  \
+        }                                                                      \
+                                                                               \
+        void vec_default_sort_##type_name(vector_##type_name *vec)             \
+        {                                                                      \
+                vec_sort_##type_name(vec, &default_condition_##type_name);     \
+        }                                                                      \
+                                                                               \
+        void vec_default_sort_interval_##type_name(vector_##type_name *vec,    \
+                                                   const int x, const int y)   \
+        {                                                                      \
+                vec_sort_interval_##type_name(                                 \
+                    vec, &default_condition_##type_name, x, y);                \
+        }                                                                      \
+                                                                               \
+        int vec_group_##type_name(vector_##type_name *vec,                     \
+                                  int (*condition)(type_name))                 \
+        {                                                                      \
+                int ret;                                                       \
+                for(int i = 0; i < vec->_vector_size - 1; i++)                 \
+                {                                                              \
+                        if(condition((vec->_vector_elements)[i]))              \
+                        {                                                      \
+                                for(int j = i; j < vec->_vector_size - 1; j++) \
+                                {                                              \
+                                        if(!condition(                         \
+                                               (vec->_vector_elements)[j]))    \
+                                        {                                      \
+                                                swap_##type_name(              \
+                                                    &(vec->_vector_elements)   \
+                                                        [i],                   \
+                                                    &(vec->_vector_elements)   \
+                                                        [j]);                  \
+                                                ret = i;                       \
+                                                break;                         \
+                                        }                                      \
+                                }                                              \
+                        }                                                      \
+                }                                                              \
+                return ret + 1;                                                \
         }
